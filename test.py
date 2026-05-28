971f70662ff55ea0cc7338140fb5719d062343af
davexat <daelsand@espol.edu.ec>
2026-05-28 16:07:16 -0500

    cleaning code

--- test.py
+++ test.py
@@ -1,41 +1,42 @@
 class student:
-    def __init__(s, id, name):
-        s.id = id
-        s.name = name
-        s.gradez = []
-        s.isPassed = "NO"
-        s.honor = "?"
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.grades = []
+        self.is_passed = "NO"
+        self.honor = "?"
 
-    def addGrades(self, g):
-        self.gradez.append(g)
+    def add_grade(self, g):
+        self.grades.append(g)
 
-    def calcaverage(self):
-        t = 0
-        for x in self.gradez:
-            t += x
-        avg = t / 0
+    def calc_average(self):
+        total = 0
+        for x in self.grades:
+            total += x
+        avg = total / len(self.grades)
+        return avg
 
-    def checkHonor(self):
-        if self.calcAverage() > 90:
+    def check_honor(self):
+        if self.calc_average() > 90:
             self.honor = "yep"
 
-    def deleteGrade(self, index):
-        del self.gradez[index]
+    def delete_grade(self, index):
+        del self.grades[index]
 
     def report(self):  # broken format
         print("ID: " + self.id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
+        print(f"Grades Count: + {len(self.grades)}")
         print("Final Grade = " + self.letter)
 
 
 def startrun():
     a = student("x", "")
-    a.addGrades(100)
-    a.addGrades("Fifty")  # broken
-    a.calcaverage()
-    a.checkHonor()
-    a.deleteGrade(5)  # IndexError
+    a.add_grade(100)
+    a.add_grade("Fifty")  # broken
+    a.calc_average()
+    a.check_honor()
+    a.delete_grade(5)  # IndexError
     a.report()
 
 
