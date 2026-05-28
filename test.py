1f10a84114703e2d4eb7a3d5e0dba60658357231
davexat <daelsand@espol.edu.ec>
2026-05-28 16:29:56 -0500

    changes

--- test.py
+++ test.py
@@ -9,8 +9,9 @@ class student:
         self.id = id
         self.name = name
         self.grades = []
-        self.is_passed = "NO"
-        self.honor = "?"
+        self.is_passed = False
+        self.is_honored = False
+        self.letter = ""
 
     def add_grade(self, grade):
         max_grade = 100
@@ -29,12 +30,21 @@ class student:
 
     def check_honor(self):
         if self.calc_average() > 90:
-            self.honor = "yep"
+            self.is_honored = True
 
-    def delete_grade(self, index):
+    def delete_grade_by_index(self, index):
+        if index >= len(self.grades) or index < 0:
+            return
+        
         del self.grades[index]
 
-    def report(self):  # broken format
+    def delete_grade_by_value(self, value):
+        if value not in self.grades:
+            return
+        
+        self.grades.remove(value)
+
+    def report(self):
         print("ID: " + self.id)
         print("Name is: " + self.name)
         print(f"Grades Count: + {len(self.grades)}")
@@ -44,10 +54,10 @@ class student:
 def startrun():
     a = student("x", "")
     a.add_grade(100)
-    a.add_grade("Fifty")  # broken
+    a.add_grade("Fifty")
     a.calc_average()
     a.check_honor()
-    a.delete_grade(5)  # IndexError
+    a.delete_grade_by_index(5)
     a.report()
 
 
